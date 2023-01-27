8ac4fa075df8c576cff5f3a39f2d7adc93b610f9
LyubovJM <kamaelresource@gmail.com>
2023-01-26 19:38:20 -0600

    Initial Commit

---
+++ CoppelMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoppelMovies
+//
+//  Created by Jesus Muñoz on 26/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
